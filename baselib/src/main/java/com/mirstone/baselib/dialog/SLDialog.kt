8a97fd6ba8c3dfeb4b11a8a8ebd96ad00f8407e3
shiliangdyh <492167662@qq.com>
2018-07-31 21:42:10 +0800

    dialogfragment

---
+++ baselib/src/main/java/com/mirstone/baselib/dialog/SLDialog.kt
@@ -0,0 +1,24 @@
+package com.mirstone.baselib.dialog
+
+import android.view.View
+
+import com.mirstone.baselib.R
+
+/**
+ * @package: com.mirstone.baselib.dialog
+ * @fileName: SLDialog
+ * @data: 2018/7/31 19:48
+ * @author: ShiLiang
+ * @describe:
+ */
+class SLDialog : BaseDialogFragment() {
+
+    override val layoutRes: Int
+        get() = R.layout.dialog_layout
+
+    override fun bindView(v: View) {
+
+    }
+
+
+}
